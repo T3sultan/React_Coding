90176382d8de583ce317e8011147cf5a6f4038f7
unknown <{T3sultan}@users.noreply.github.com>
2023-02-13 09:02:22 +0600

    download

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Index from "./components/Index";
+import Menu from "./components/Menu";
+import Problem1 from "./components/Problem-1";
+import Problem2 from "./components/Problem-2";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/" element={<Menu />}>
+          <Route path="problem-1" element={<Problem1 />} />
+          <Route path="problem-2" element={<Problem2 />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
